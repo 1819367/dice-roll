9b9205b7771609d26fde377cbc2a1e2b9d592e7f
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-26 14:09:58 -0700

    generate new dice

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import RollDice from './components/RollDice'
 
 export default function App() {
 
+//generate two dice with a random side name
+  function generateAllNewDice() {
+  const sides = ['one', 'two', 'three', 'four', 'five', 'six']
+  return new Array(2) //creates a new array of length 2
+    .fill(0) //fill it with zeros
+    .map(() => sides[Math.floor(Math.random() * sides.length)]) //map over the array
+}
+  console.log(generateAllNewDice())
+
   return (
     <main>
       <div className='dice-container'>
