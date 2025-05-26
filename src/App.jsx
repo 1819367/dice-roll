06b139dfd7b09dcc38b81ebe6c32f1b18152dce1
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-26 14:27:02 -0700

    generate components array

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,26 @@ import RollDice from './components/RollDice'
 
 export default function App() {
 
+  //state to hold array of dice
+  const [ dice, setDice ] = useState(generateAllNewDice());
+
 //generate two dice with a random side name
   function generateAllNewDice() {
-  const sides = ['one', 'two', 'three', 'four', 'five', 'six']
-  return new Array(2) //creates a new array of length 2
-    .fill(0) //fill it with zeros
-    .map(() => sides[Math.floor(Math.random() * sides.length)]) //map over the array
-}
-  console.log(generateAllNewDice())
+    const sides = ['one', 'two', 'three', 'four', 'five', 'six']
+    return new Array(2) //creates a new array of length 2
+      .fill(0) //fill it with zeros
+      .map(() => sides[Math.floor(Math.random() * sides.length)]) //map over the array
+  }
+  // console.log(generateAllNewDice()) //console log to call new dice array
+
+  const diceElements = dice.map((die) => (
+    <Die value={die} />
+  ))
 
   return (
     <main>
       <div className='dice-container'>
-        <Die value={1}/>
-        <Die value={2}/>
+        {diceElements}
       </div>
         <RollDice />
     </main>
